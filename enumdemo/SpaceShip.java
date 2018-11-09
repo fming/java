e0835ee47c7cdb0e3a047c20ab02c23b1d801eed
mingfang <ming_fang@keysight.com>
2018-11-09 13:29:04 +0800

    override enum

--- enumdemo/SpaceShip.java
+++ enumdemo/SpaceShip.java
@@ -1,1 +1,18 @@
-public enum
+/**
+ * Overriding enum methods
+ */
+public enum SpaceShip {
+    SCOUT, CARGO, TRANSPORT, CRUISER, BATTLESHIP, MOTHERSHIP;
+
+    public String toString() {
+        String id = name();
+        String lowver = id.substring(1).toLowerCase();
+        return id.charAt(0) + lowver;
+    }
+
+    public static void main(String[] args) {
+        for(SpaceShip s : values()) {
+            System.out.println(s);
+        }
+    }
+}
